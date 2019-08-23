78801753401e1ebc3d6b77bb4f6ba17567206789
zhonghua-dev <lzhontheway@163.com>
2019-08-23 20:15:56 +0800

    init demo

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '@/views/login'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/login', name: 'login', component: Login }
+  ]
+})
+export default router
